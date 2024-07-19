6768c20c58690a80a09303f03ec2aa3d719f848c
Googolplexo <i.a.kokarev@yandex.ru>
2024-07-19 21:01:57 +0500

    Removed Galaxy scaling text

--- src/components/tabs/antimatter-dimensions/ClassicAntimatterGalaxyRow.vue
+++ src/components/tabs/antimatter-dimensions/ClassicAntimatterGalaxyRow.vue
@@ -70,7 +70,7 @@ export default {
     costScalingText() {
       switch (this.type) {
         case GALAXY_TYPE.DISTANT:
-          return `Each Galaxy is more expensive past ${quantifyInt("Galaxy", this.distantStart)}`;
+          return "";
         case GALAXY_TYPE.REMOTE: {
           const scalings = [
             { type: "distant", function: "quadratic", amount: this.distantStart },
