daaab9c6450c2ba5d1ce82a13dc8a36bf7ad5059
Googolplexo <i.a.kokarev@yandex.ru>
2024-07-30 15:09:13 +0500

    .

--- src/core/storage/migrations.js
+++ src/core/storage/migrations.js
@@ -426,7 +426,7 @@ export const migrations = {
     102: player => {
       for (let tier = 1; tier < 9; tier++) {
         const dim = InfinityDimension(tier);
-        player.dimensions.infinity[tier].cost = dim.costMultiplier.toDecimal().pow(player.dimensions.infinity[tier].amount).times(dim.baseCost);
+        if (dim.isUnlocked) player.dimensions.infinity[tier].cost = dim.costMultiplier.toDecimal().pow(player.dimensions.infinity[tier].amount).times(dim.baseCost);
       }
     }
   },
